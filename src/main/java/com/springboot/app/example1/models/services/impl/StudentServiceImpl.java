3b382bcf8d816785fdddf3b4e342ad9938447158
shahidul <er.shahidul@gmail.com>
2022-12-27 12:45:57 +0600

    code refactor

--- src/main/java/com/springboot/app/example1/models/services/impl/StudentServiceImpl.java
+++ src/main/java/com/springboot/app/example1/models/services/impl/StudentServiceImpl.java
@@ -1,12 +1,12 @@
 package com.springboot.app.example1.models.services.impl;
 
-import com.springboot.app.example1.models.Student;
-import com.springboot.app.example1.models.repositories.StudentRepository;
-import com.springboot.app.example1.models.services.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
+import org.springframework.stereotype.Service;
+import com.springboot.app.example1.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.springboot.app.example1.models.services.StudentService;
+import com.springboot.app.example1.models.repositories.StudentRepository;
 
 @Service
 public class StudentServiceImpl implements StudentService {
